e8c05739701f310140bde2c2241b17e61f9d0311
Krzysztof Osmaek <krzys.osmalek@gmail.com>
2025-05-01 15:04:25 +0200

    Re-added kodilla-patterns as regular directory

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/tasks/TaskFactoryTestSuite.java
@@ -0,0 +1,51 @@
+package com.kodilla.patterns.tasks;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+class TaskFactoryTestSuite {
+
+    @Test
+    void testFactoryShoppingTask() {
+        // Given
+        TaskFactory factory = new TaskFactory();
+
+        // When
+        Task shopping = factory.makeTask(TaskFactory.SHOPPING);
+        shopping.executeTask();
+
+        // Then
+        assertEquals("Grocery shopping", shopping.getTaskName());
+        assertTrue(shopping.isTaskExecuted());
+    }
+
+    @Test
+    void testFactoryPaintingTask() {
+        // Given
+        TaskFactory factory = new TaskFactory();
+
+        // When
+        Task painting = factory.makeTask(TaskFactory.PAINTING);
+        painting.executeTask();
+
+        // Then
+        assertEquals("Wall painting", painting.getTaskName());
+        assertTrue(painting.isTaskExecuted());
+    }
+
+    @Test
+    void testFactoryDrivingTask() {
+        // Given
+        TaskFactory factory = new TaskFactory();
+
+        // When
+        Task driving = factory.makeTask(TaskFactory.DRIVING);
+        driving.executeTask();
+
+        // Then
+        assertEquals("Business trip", driving.getTaskName());
+        assertTrue(driving.isTaskExecuted());
+    }
+}
